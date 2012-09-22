7cf79f93314013e25cbfbfed3e48abd3d976ef98
master994 <master994@gmail.com>
2012-09-22 18:00:39 +0200

    Első verzió!

---
+++ Main/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+int main(){
+
+int napokSzama;
+int korlat;
+int napok[100];
+int i,j;
+int legmelegebbHomerseglet = 0;
+int legmelegebbNap;
+int korlatnalMagasabb = 0;
+int idoszak = 0;
+int kezdoNap = 0;
+int vegsoNap = 0;
+
+// Kezdő adatok beolvasása
+
+printf( "Napok szama:\n" );
+scanf( "%d" , &napokSzama);
+printf( "Korlat:\n" );
+scanf( "%d" ,&korlat );
+
+
+// Napok homersegletenek beolvasa
+
+for( i = 0; i < napokSzama; i++){
+    printf("%d. nap:",i+1);
+	scanf( "%d" , &napok[i] );
+	printf("\n");
+}
+
+// 1. feladat : Legmelegebb nap meghatározása
+
+for( i = 0; i < napokSzama; i++){
+	if( napok[i] > legmelegebbHomerseglet ){
+		legmelegebbHomerseglet = napok[i];
+		legmelegebbNap = i;
+	}
+}
+printf( "Legmelegebb nap: %d\n",legmelegebbNap+1);
+
+// 2. feladat : Korlátnál melegebb napok száma
+
+for( i = 0; i < napokSzama; i++){
+	if( napok[i] > korlat ){
+		korlatnalMagasabb++;
+	}
+}
+printf( "K-nal magasabb napok szama: %d\n",korlatnalMagasabb);
+
+// 3. feladat : Leghoszabb korlat feletti időszak
+
+for( i = 0; i < napokSzama; i++){
+	if( napok[i] > korlat ){
+		idoszak = 0;
+		for( j = i; napok[j] > korlat; j++){
+				idoszak++;
+		}
+		
+		if( idoszak > ( ( vegsoNap + 1 ) - ( kezdoNap + 1 ) ) ){
+			kezdoNap = i;
+			vegsoNap = j;
+		} 
+		i = j;
+	}
+}
+if( kezdoNap != 0 && vegsoNap != 0)
+        printf( "Legmelegebb korlatnal magasabb idoszak: %d. - %d. nap",kezdoNap+1,vegsoNap);
+else
+    printf("Nincs korlat feletti homerseglet.");
+return 0;
+
+}
